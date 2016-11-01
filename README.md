e404a0b8a54e805139f9ae5f65c8a797b6c3a2cf
Steven Smith <ssmith@perdixsw.com>
2016-11-01 17:33:39 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # umpy
 Simple unit of measure conversion for python, optimized for manufacturing contexts
+
+NOTE:  This is extremely young, buggy software. Pull requests are welcome!
---
+++ setup.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+from setuptools import setup
+
+setup(
+	name='umpy',
+	version='0.0.1',
+	url='https://www.github.com/perdixsw/umpy',
+	author='Perdix Software',
+	author_email='ssmith@perdixsw.com',
+	description=('Simple unit of measure conversion for python, optimized for manufacturing contexts.'),
+	license='MIT',
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[],
+	classifiers=[
+		'Development Status :: 2 - Pre-Alpha',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+		'Topic :: Office/Business :: Financial',
+		'Topic :: Scientific/Engineering :: Mathematics',
+	],
+)
